53f4c97af071b4e2bb104a151d22d281f8849760
arvils <arvilslud1@gmail.com>
2025-01-18 17:00:25 +0200

    fixed install script, added installation of packages

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-mkdir -p "$CONFIG_DIR"
-DOTFILES_DIR=~/source-code/.dotfiles
-CONFIG_DIR=~/.config
 
-FILES=($(ls /home/user/dotfiles))  # Automatically list all files in the directory
+#Installing packages
+echo "Updateing system..."
+sudo pacman -Syu --noconfirm
+
+echo "Installing packages..."
+sudo pacman -S alacritty --noconfirm
+sudo pacman -S neovim --noconfirm
+sudo pacman -S tmux --noconfirm
+
+USER_HOME=$(eval echo ~$SUDO_USER)
+DOTFILES_DIR="$USER_HOME/source-code/.dotfiles"
+CONFIG_DIR="$USER_HOME/.config"
+
+FILES=($(ls $DOTFILES_DIR))  # Automatically list all files in the directory
 
 for FILE in "${FILES[@]}"; do
     ln -sfn "$DOTFILES_DIR/$FILE" "$CONFIG_DIR/$FILE"
