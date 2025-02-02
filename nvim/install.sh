66ad28ced81be91314253ecdf3361644a74fcba3
arvils <arvilslud1@gmail.com>
2025-02-02 21:22:24 +0200

    added test

---
+++ nvim/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Installing packages
+echo "Updateing system..."
+sudo pacman -Syu --noconfirm
+
+echo "Installing packages..."
+sudo pacman -S alacritty --noconfirm
+sudo pacman -S neovim --noconfirm
+sudo pacman -S tmux --noconfirm
+
+USER_HOME=$(eval echo ~$SUDO_USER)
+DOTFILES_DIR="$USER_HOME/source-code/.dotfiles"
+CONFIG_DIR="$USER_HOME/.config"
+
+FILES=($(ls $DOTFILES_DIR))  # Automatically list all files in the directory
+
+for FILE in "${FILES[@]}"; do
+    if [ -d "$DOTFILES_DIR/$FILE" ]; then
+        ln -sfn "$DOTFILES_DIR/$FILE" "$CONFIG_DIR/$FILE"
+        echo "Linked $DOTFILES_DIR/$FILE -> $CONFIG_DIR/$FILE"
+    else 
+        echo "Skipping (not a directory): $DOTFILES_DIR/$FILE"
+    fi
+done
+
