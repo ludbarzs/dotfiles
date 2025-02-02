2e6bb5a315fc6c413ed6342648788477274d511a
arvils <arvilslud1@gmail.com>
2025-02-02 19:07:18 +0200

    Fixed install script (files have to be deleted in the /home/arg/.config to be symlinked

--- install.sh
+++ install.sh
@@ -16,7 +16,11 @@ CONFIG_DIR="$USER_HOME/.config"
 FILES=($(ls $DOTFILES_DIR))  # Automatically list all files in the directory
 
 for FILE in "${FILES[@]}"; do
-    ln -sfn "$DOTFILES_DIR/$FILE" "$CONFIG_DIR/$FILE"
-    echo "Linked $DOTFILES_DIR/$FILE -> $CONFIG_DIR/$FILE"
+    if [ -d "$DOTFILES_DIR/$FILE" ]; then
+        ln -sfn "$DOTFILES_DIR/$FILE" "$CONFIG_DIR/$FILE"
+        echo "Linked $DOTFILES_DIR/$FILE -> $CONFIG_DIR/$FILE"
+    else 
+        echo "Skipping (not a directory): $DOTFILES_DIR/$FILE"
+    fi
 done
 
