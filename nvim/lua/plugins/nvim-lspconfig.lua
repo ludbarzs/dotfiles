1fa4027074c0e6876af69952711829e2d2da2f00
arvils <arvilslud1@gmail.com>
2025-02-04 09:42:25 +0200

    Added nvim plugins for typescript

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -163,9 +163,10 @@ return {
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
+			-- tsserver = {},
 			-- clangd = {},
 			-- gopls = {},
-			-- pyright = {},
+			pyright = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
@@ -174,8 +175,6 @@ return {
 			--
 			-- But for many setups, the LSP (`ts_ls`) will work just fine
 			-- ts_ls = {},
-			--
-
 			lua_ls = {
 				-- cmd = { ... },
 				-- filetypes = { ... },
