a8447d4ec487b5b095cba4defc442a8dfc79fb41
AL <arvilslud@gmail.com>
2025-07-06 21:37:00 +0300

    Properly deleted image.nvim

--- nvim/lua/ludbarzs/plugins/neorg.lua
+++ nvim/lua/ludbarzs/plugins/neorg.lua
@@ -2,8 +2,8 @@ return {
   "nvim-neorg/neorg",
   -- If not work run the command: Neorg sync-parsers
   build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim", "3rd/image.nvim" },
-  lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+  dependencies = { "nvim-lua/plenary.nvim" },
+  lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = "*", -- Pin Neorg to the latest stable release
   opts = {
     load = {
