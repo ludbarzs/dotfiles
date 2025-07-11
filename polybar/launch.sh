8e3269f1dcbd8a42c64cd0f498694b453f6e71a6
Arvils Ludbarzs <arvilslud1@gmail.com>
2025-07-11 23:05:43 +0300

    theme: onedark

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,7 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar gruvbox 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar polybar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Bars launched..."
